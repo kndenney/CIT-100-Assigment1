41adea9de5c30dabcb9faab0d7fe4b3055b77a04
Kyle Denney <kndenney@gmail.com>
2018-08-13 13:31:18 -0400

    Add project files.

---
+++ CIT-100-Assigment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CIT_100_Assigment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
